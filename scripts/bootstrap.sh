007c29aa6a674990d840bd8a456b64773bcc7eea
MegChai <acdoge.cao@gmail.com>
2021-10-25 16:25:18 +0800

    feat: update bootstrap scropt for macosx system

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 set -e
 
-sudo apt install git-lfs
+# TODO: Support for more systems and linux distributions
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo apt install -y git-lfs pandoc graphviz
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install git-lfs pandoc graphviz
+else
+    echo "Not support now."
+fi
+
 git lfs install
 git submodule update --init --progress --depth=1 --recursive
 python3 -m pip install --user -r requirements.txt
 
-# TODO: Support for more systems and linux distributions
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo apt install -y pandoc graphviz
-else
-    echo "Not support now."
-fi
 
