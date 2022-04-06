907046af786d77c707f137b0d134ee429d016f00
MegChai <acdoge.cao@gmail.com>
2022-04-06 15:33:32 +0800

    ci: stritc check for building zh website

--- scripts/oss/build_for_oss.sh
+++ scripts/oss/build_for_oss.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-make BUILDDIR=/tmp LANGUAGE="zh_CN" html --keep-going
+make BUILDDIR=/tmp LANGUAGE="zh_CN" html
 mv /tmp/html zh
-make BUILDDIR=/tmp LANGUAGE="en" html --keep-going
+make BUILDDIR=/tmp LANGUAGE="en" html
 mv /tmp/html en
 tar czf static.tgz zh en
 rm -r zh en
