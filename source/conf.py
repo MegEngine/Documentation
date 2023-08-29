dd20e2947d858640384295cfa7f83f491d42d657
Wanwan1996 <17733781361@163.com>
2023-08-29 11:34:59 +0800

    update version of dev

--- source/conf.py
+++ source/conf.py
@@ -75,7 +75,7 @@ elif doc_mode == "FULL":
 project = "MegEngine"
 copyright = f"2020-{datetime.now().year}, The MegEngine Open Source Team"
 author = "The MegEngine Open Source Team"
-version = "1.13.1"
+version = "1.13.2"
 release = version
 
 # -- Version Check -----------------------------------------------------------
